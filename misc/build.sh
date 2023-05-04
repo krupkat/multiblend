4005696c55a3ce15417917a0f3e96ac40036ac34
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2023-05-04 21:57:34 +0200

    parallel linux build

--- misc/build.sh
+++ misc/build.sh
@@ -2,6 +2,6 @@ cmake -B build \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake"
 
-cmake --build build
+cmake --build build -j $(nproc)
 
 ctest --test-dir build -V
