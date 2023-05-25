dea95306b7e44206c0001f67b402fe23670b4459
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2023-05-25 19:04:30 +0200

    add MultiblendLib test for in memory usage

--- tests/multiblend_test.cpp
+++ tests/multiblend_test.cpp
@@ -1,7 +1,38 @@
 #include "src/multiblend.h"
 
+#include <fstream>
+#include <vector>
+
 #include <catch2/catch_test_macros.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/vector.hpp>
+
+#include "src/image.h"
+
+multiblend::io::Image Load(const char* path) {
+  std::ifstream ifs(path, std::ios::binary);
+  cereal::BinaryInputArchive archive(ifs);
+
+  multiblend::io::InMemoryImage image;
+  archive(image);
+  return multiblend::io::Image(image);
+}
 
 TEST_CASE("MultiblendLibTest") {
-    REQUIRE(1 == 1);
+  std::vector<multiblend::io::Image> images;
+  images.push_back(Load("data/img_0.png.raw"));
+  images.push_back(Load("data/img_1.png.raw"));
+  images.push_back(Load("data/img_2.png.raw"));
+
+  multiblend::Options options{
+      .output_type = multiblend::io::ImageType::MB_IN_MEMORY, .output_bpp = 8};
+
+  auto result = multiblend::Multiblend(images, options);
+
+  CHECK(result.width == 331);
+  CHECK(result.height == 244);
+  CHECK(result.output_bpp == 8);
+  CHECK(result.no_mask == false);
+  CHECK(result.min_xpos == -167);
+  CHECK(result.min_ypos == 193);
 }
