211cabcf05a7018263b9143939a5c074f72bb30d
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2023-06-15 10:46:12 +0200

    improve arm options in cmakelists

--- misc/arm_linux_build.sh
+++ misc/arm_linux_build.sh
@@ -3,9 +3,9 @@ export VCPKG_FORCE_SYSTEM_BINARIES=1 # needed for vcpkg on arm
 cmake -B build \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake" \
-  -DVCPKG_TARGET_TRIPLET="arm64-osx" \
+  -DVCPKG_TARGET_TRIPLET="arm64-linux" \
   -DMULTIBLEND_ARM_OPTIMIZED=ON
 
-cmake --build build -j 6
+cmake --build build -j 4
 
 ctest --test-dir build -V
