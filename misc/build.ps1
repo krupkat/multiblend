6b4e9435849d313faa980e990ce5f7d9dbdedc96
Your Name <6817216+krupkat@users.noreply.github.com>
2023-07-28 15:29:33 +0200

    fix clean build on windows

--- misc/build.ps1
+++ misc/build.ps1
@@ -1,7 +1,8 @@
 cmake -B build -G Ninja `
   -DCMAKE_BUILD_TYPE=Release `
   -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake" `
-  -DVCPKG_TARGET_TRIPLET="x64-windows-static"
+  -DVCPKG_TARGET_TRIPLET="x64-windows-static" `
+  -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded"
 
 cmake --build build
 
